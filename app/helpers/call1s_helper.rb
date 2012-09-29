d346ebab0ff7c7ebdcf016059c0933c22caa81a9
Eric Barrett <ericnbarrett@gmail.com>
2012-09-29 18:08:47 +0400

    Created separate page for missing precinct table

--- app/helpers/call1s_helper.rb
+++ app/helpers/call1s_helper.rb
@@ -1,2 +1,14 @@
+# encoding: utf-8
 module Call1sHelper
+  
+  def yn_label(value)
+    if value.nil?
+      ''
+    elsif value == true
+      'დიახ'
+    else value == false
+      'არა'
+    end
+  end
+
 end
