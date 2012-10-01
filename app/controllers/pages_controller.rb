84b864065f0bfe8a3cd79e6ef800a6fb141c197d
Eric Barrett <ericnbarrett@gmail.com>
2012-10-01 08:12:16 +0400

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
+  
   before_filter do |controller_instance|
-    controller_instance.send(:valid_role?, :admin)
+    controller_instance.send(:valid_role?, :author)
   end
 
   def home
