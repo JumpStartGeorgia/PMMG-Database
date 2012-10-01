bcdf39952e3e17f503e3d1b5ca3531d4b95b7e14
Eric Barrett <ericnbarrett@gmail.com>
2012-10-01 09:46:58 +0400

    fixed missing p bug

--- app/models/precinct.rb
+++ app/models/precinct.rb
@@ -1,6 +1,8 @@
 class Precinct < ActiveRecord::Base
   attr_accessible :district_id, :pname
   
+  validates :district_id, :pname, :presence=>true
+  
   belongs_to :district
   has_many :call1s
   has_many :call2s
@@ -14,6 +16,7 @@ class Precinct < ActiveRecord::Base
 	
   def self.missing_call(objname)
    call = objname.select("precinct_id")
+#   call = call.delete_if { |k,v| v.nil?}
    return self.where("id not in (?)", call.collect(&:precinct_id))
   end
   
